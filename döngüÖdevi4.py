1e849108ae7ad90508ba081f41c5810de3f982f1
Deniz Ceren Demir <93373988+denizcerendemir@users.noreply.github.com>
2022-01-25 15:30:28 +0300

    Add files via upload

---
+++ döngüÖdevi4.py
@@ -0,0 +1,20 @@
+"""Problem 4
+Her bir while döngüsünde kullanıcıdan bir sayı alın ve kullanıcının girdiği sayıları "toplam" isimli bir değişkene ekleyin. Kullanıcı "q" tuşuna bastığı zaman döngüyü sonlandırın ve ekrana "toplam değişkenini" bastırın.
+
+İpucu : while döngüsünü sonsuz koşulla başlatın ve kullanıcı q'ya basarsa döngüyü break ile sonlandırın."""
+print("""
+********************************************************
+Toplamak istediğiniz sayıları girin
+Programdan çıkma için 'q' harfine basmanız yeterlidir.
+********************************************************
+""")
+toplam=0
+ts=0
+while (True):
+    ts=int(input("Sayıyı giriniz : "))
+    toplam +=ts
+    print(toplam)
+    if ts=="q" :
+        break
+
+
